629e4fc8bbc33df9427fd88c993c1557d680d382
Will Harris-Braun <willharrisbraun@gmail.com>
2021-04-13 11:31:36 -0400

    Add welcome comment in Bash script since that's what opens in replit

--- main.sh
+++ main.sh
@@ -1,1 +1,20 @@
+# -----------------------------
+# QUEST FOR THE OJERAN GEMERALD
+# -----------------------------
+#
+# Quest for the Ojeran Gemerald is a text-based, role-playing, adventure game.
+# Use the Run button up there ^^ to start the game.
+#
+# Instructions:
+# In the game, you use text commands to move around and interact with the world.
+# For example, use the command "n" to move North, "u" to move up, etc. Playing
+# with a pad of paper and a pencil nearby might be helpful.
+#
+# Using the command "help" will give you a list of commands and what they do.
+# If you type "help" and then one of the commands on the list, it will give
+# you detailed information on that command
+#
+# https://github.com/qubist/qog
+#
+
 clj main.clj
